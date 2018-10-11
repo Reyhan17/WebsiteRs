3bdeabc0ab75a16b6a5d45ed6541f74b3becd60c
MochamadFikri <mochamadfikri82@gmail.com>
2018-10-11 10:48:58 +0700

    penambahan logo dan penyuntingan halaman berita

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -19,11 +19,64 @@
   </head>
 
   <body>
+    <style media="screen">
+      .f{
+        margin-top: -8px;
+      }
+      .f_berita{
+        font-size: 20px;
+        font-weight: bold;
+      }
+      form_main {
+    width: 100%;
+}
+.form_main h4 {
+    font-family: roboto;
+    font-size: 20px;
+    font-weight: 300;
+    margin-bottom: 15px;
+    margin-top: 20px;
+    text-transform: uppercase;
+}
+.form {
+    border-radius: 7px;
+    padding: 6px;
+}
+.txt[type="text"] {
+    border: 1px solid #ccc;
+    margin: 10px 0;
+    padding: 10px 0 10px 5px;
+    width: 100%;
+}
+.txt_3[type="text"] {
+    margin: 10px 0 0;
+    padding: 10px 0 10px 5px;
+    width: 100%;
+}
+.txt2[type="submit"] {
+    background: #242424 none repeat scroll 0 0;
+    border: 1px solid #4f5c04;
+    border-radius: 25px;
+    color: #fff;
+    font-size: 16px;
+    font-style: normal;
+    line-height: 35px;
+    margin: 10px 0;
+    padding: 0;
+    text-transform: uppercase;
+    width: 30%;
+}
+.txt2:hover {
+    background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
+    color: #5793ef;
+    transition: all 0.5s ease 0s;
+}
 
+    </style>
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">RS Firmosa</a>
+        <a class="navbar-brand" href="#"><img class="f" src="<?php echo base_url() ?>assets/images/firmosa.png" alt="f" width="50" height="50px">Firmosa Medical Center</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -54,7 +107,7 @@
       <div class="row">
 
         <div class="col-lg-3">
-          <h1 class="my-4">Halaman Berita</h1>
+          <h1 class="my-4">Berita</h1>
           <div class="list-group">
             <a href="#" class="list-group-item">Lihat Berita</a>
             <a href="#" class="list-group-item active">Tambah Berita</a>
@@ -67,21 +120,24 @@
 
           <div class="card mt-4">
             <div class="card-body">
-              <h3 class="card-title">Product Name</h3>
-              <h4>$24.99</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
-              <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
-              4.0 stars
+              <h3 class="card-title">Tambah Berita</h3>
+                <form class="" action="#" method="post">
+                  <br>
+                  <input type="text" required placeholder="Masukan Judul Berita" name="judul" class="txt">
+                	<textarea placeholder="Masukan Isi Berita" name="isi" type="text" class="txt_3"></textarea>
+                  <input type="submit" value="submit" name="submit" class="txt2">
+                </form>
             </div>
           </div>
           <!-- /.card -->
 
           <div class="card card-outline-secondary my-4">
             <div class="card-header">
-              Product Reviews
+              Berita Terbaru
             </div>
             <div class="card-body">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              <p>Polisi akan libatkan Ikatan Dokter Indonesia (IDI) untuk mengusut kasus dugaan malpraktik yang dilakukan sebuah rumah sakit swasta di Sidoarjo, Jawa Timur.
+Seperti ditayangkan Liputan6 Pagi SCTV, Rabu (31/1/2018), pihak rumah sakit membantah tuduhan tersebut dan menyatakan bahwa video dugaan malpraktik yang menjadi viral adalah hoax.</p>
               <small class="text-muted">Posted by Anonymous on 3/1/17</small>
               <hr>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
